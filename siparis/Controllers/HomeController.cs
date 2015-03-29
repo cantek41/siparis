13d3a68e4aa2eedae5943d7d144916b8ddee0c90
issobaba <ismailferik1963@gmail.com>
2015-03-29 11:14:17 +0300

    index  degistirildi contact sayfasi ayarlandi

--- siparis/Controllers/HomeController.cs
+++ siparis/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace siparis.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+           // ViewBag.Message = "Your contact page.";
 
             return View();
         }
