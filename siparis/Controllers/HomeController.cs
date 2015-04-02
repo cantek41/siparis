33a85dfa197e110b8126339f979cc0655df16312
cantek41 <cantek41@gmail.com>
2015-04-02 18:55:30 +0300

    sepete ekle

--- siparis/Controllers/HomeController.cs
+++ siparis/Controllers/HomeController.cs
@@ -15,12 +15,68 @@ namespace siparis.Controllers
         public ActionResult Index()
         {
             checkCart();
+            //VdbSoftEntities db = new VdbSoftEntities();
+            //var model = from d in db.STOKCARDs
+            //            select d;
+            //return View(model);
+
             VdbSoftEntities db = new VdbSoftEntities();
-            var model = from d in db.STOKCARDs
-                        select d;
+
+            SortingPagingInfo info = new SortingPagingInfo();
+            info.SortField = "ID";
+            info.SortDirection = "ascending";
+            info.PageSize = 6;
+            info.PageCount = Convert.ToInt32(Math.Ceiling((double)(db.STOKCARDs.Count()
+                           / info.PageSize)));
+            info.CurrentPageIndex = 0;
+            var query = db.STOKCARDs.OrderBy(c => c.ID).Take(info.PageSize);
+            ViewBag.SortingPagingInfo = info;
+            List<STOKCARD> model = query.ToList();
             return View(model);
+
         }
 
+        /// <summary>
+        /// sayfa atlatma
+        /// </summary>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(SortingPagingInfo info)
+        {
+            VdbSoftEntities db = new VdbSoftEntities();
+          
+                IQueryable<STOKCARD> query = null;
+                switch (info.SortField)
+                {
+                    case "ID":
+                        query = (info.SortDirection == "ascending" ?
+                                 db.STOKCARDs.OrderBy(c => c.ID) :
+                                 db.STOKCARDs.OrderByDescending(c => c.ID));
+                        break;
+                    //case "CompanyName":
+                    //    query = (info.SortDirection == "ascending" ?
+                    //             db.STOKCARDs.OrderBy(c => c.CompanyName) :
+                    //             db.STOKCARDs.OrderByDescending(c => c.CompanyName));
+                    //    break;
+                    //case "ContactName":
+                    //    query = (info.SortDirection == "ascending" ?
+                    //             db.STOKCARDs.OrderBy(c => c.ContactName) :
+                    //             db.STOKCARDs.OrderByDescending(c => c.ContactName));
+                    //    break;
+                    //case "Country":
+                    //    query = (info.SortDirection == "ascending" ?
+                    //             db.STOKCARDs.OrderBy(c => c.Country) :
+                    //             db.STOKCARDs.OrderByDescending(c => c.Country));
+                        //break;
+                }
+                query = query.Skip(info.CurrentPageIndex
+                      * info.PageSize).Take(info.PageSize);
+                ViewBag.SortingPagingInfo = info;
+                List<STOKCARD> model = query.ToList();
+                return View(model);
+            
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
