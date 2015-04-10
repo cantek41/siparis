f79a8a8375bccab3d88c57fc85ba6f04000c4ffa
issobaba <ismailferik1963@gmail.com>
2015-04-10 10:12:52 +0300

    addstok sayfasi tasarimi duzenlendi

--- siparis/Controllers/StokController.cs
+++ siparis/Controllers/StokController.cs
@@ -15,40 +15,42 @@ namespace siparis.Controllers
         public ActionResult AddStok()
         {
             VdbSoftEntities db = new VdbSoftEntities();
-            TempData["Category"] =new SelectList(from d in db.STOKGROUPs
-                                   select new { Key = d.STOK_GROUP_CODE, Text = d.NAME_TR });
+            TempData["Category"] = new SelectList(from d in db.STOKGROUPs
+                                                  select new { Key = d.STOK_GROUP_CODE, Text = d.NAME_TR });
             TempData["Brand"] = new SelectList(from d in db.STOKBRANDs
-                                select new { Key = d.BRAND_CODE, Text = d.NAME_TR });
+                                               select new { Key = d.BRAND_CODE, Text = d.NAME_TR });
             TempData["Color"] = new SelectList(from d in db.STOKCOLORs
-                                select new { Key = d.COLOR_CODE, Text = d.NAME_TR });
+                                               select new { Key = d.COLOR_CODE, Text = d.NAME_TR });
             TempData["Season"] = new SelectList(from d in db.STOKSEASONs
-                                select new { Key = d.SEASON_CODE, Text = d.NAME_TR });
+                                                select new { Key = d.SEASON_CODE, Text = d.NAME_TR });
             TempData["Body"] = new SelectList(from d in db.STOKBODies
-                                select new { Key = d.BODY_CODE, Text = d.NAME_TR });
+                                              select new { Key = d.BODY_CODE, Text = d.NAME_TR });
             return View();
         }
 
         [HttpPost]
         public ActionResult AddStok(STOKCARD stok)
         {
+            VdbSoftEntities db = new VdbSoftEntities();
+            stok.ID = db.STOKCARDs.Where(x => x.CODE == stok.CODE).Select(x => x.ID).FirstOrDefault();
+            if (stok.ID!=0)
+            {
+                return EditStok(stok);
+            }          
 
-            VdbSoftEntities db = new VdbSoftEntities();           
-           
-                stok.CREATE_DATE = DateTime.Now;
-                stok.LAST_UPDATE = DateTime.Now;
-                stok.CREATE_USER = "1";// fix me
-                stok.LAST_UPDATE_USER = "1";//fix me
-                db.STOKCARDs.Add(stok);                
-                db.SaveChanges();
-
-                ViewDataDoldur();
-            
+            stok.CREATE_DATE = DateTime.Now;
+            stok.LAST_UPDATE = DateTime.Now;
+            stok.CREATE_USER = "1";// fix me
+            stok.LAST_UPDATE_USER = "1";//fix me
+            db.STOKCARDs.Add(stok);
+            db.SaveChanges();
+            ViewDataDoldur();
             return View(stok);
         }
 
         public void ViewDataDoldur()
         {
-            VdbSoftEntities db = new VdbSoftEntities(); 
+            VdbSoftEntities db = new VdbSoftEntities();
             TempData["Category"] = new SelectList(from d in db.STOKGROUPs
                                                   select new { Key = d.STOK_GROUP_CODE, Text = d.NAME_TR });
             TempData["Brand"] = new SelectList(from d in db.STOKBRANDs
@@ -60,6 +62,17 @@ namespace siparis.Controllers
             TempData["Body"] = new SelectList(from d in db.STOKBODies
                                               select new { Key = d.BODY_CODE, Text = d.NAME_TR });
         }
-            
-	}
+
+        public ActionResult EditStok(STOKCARD stok)
+        {
+            VdbSoftEntities db = new VdbSoftEntities();
+            db.STOKCARDs.Attach(stok);
+            db.Entry(stok).State = EntityState.Modified;
+            db.SaveChanges();
+            ViewDataDoldur();
+            return View(stok);
+
+        }
+
+    }
 }
