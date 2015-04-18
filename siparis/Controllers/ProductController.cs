b3d00e816cec5f7829152257c1acf252d797a759
fsahinbas <fsahinbas@gmail.com>
2015-04-18 11:41:06 +0300

    GetColor metotu yazıldı

--- siparis/Controllers/ProductController.cs
+++ siparis/Controllers/ProductController.cs
@@ -13,25 +13,37 @@ namespace siparis.Controllers
         // GET: /Product/
         public ActionResult ProductDetail(int urunID)
         {
-            ViewDataDoldur();
             STOKCARD model = getProduct(urunID);
+            //ViewDataDoldur(model);
+            //return View(model);
+            GetColor(model);
             return View(model);
         }
 
-        public void ViewDataDoldur()
-        {
-            VdbSoftEntities db = new VdbSoftEntities();
+        //public void ViewDataDoldur(STOKCARD model)
+        //{
+        //    VdbSoftEntities db = new VdbSoftEntities();
            
                                 
-            ViewData["Color"] = (from d in db.STOKCOLORs
-                                 select new { Key = d.COLOR_CODE, Text = d.NAME_TR });
+        //    ViewData["Color"] = (from d in db.STOKCOLORs
+        //                         select new { Key = d.COLOR_CODE, Text = d.NAME_TR });
             
-            ViewData["Body"] = (from d in db.STOKBODies
-                                select new { Key = d.BODY_CODE, Text = d.NAME_TR });
-           
-           
+        //    ViewData["Body"] = (from d in db.STOKBODies
+        //                        select new { Key = d.BODY_CODE, Text = d.NAME_TR });
 
+          
+        //}
+
+        public void GetColor(STOKCARD model) 
+        {
+            VdbSoftEntities db = new VdbSoftEntities();
+            ViewData["Color"] = (from s in db.STOKCARDs
+                                 where s.UPPER_CODE == model.UPPER_CODE
+                                 join c in db.STOKCOLORs on s.COLOR_CODE equals c.COLOR_CODE
+                                 select new { Key = c.COLOR_CODE, Text = c.NAME_TR });
         }
+
+
 
     }
 }
