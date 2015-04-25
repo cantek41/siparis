0cfd00564f9fb62c861686b52de9b7a6e73b7f17
cantek41 <cantek41@gmail.com>
2015-04-25 16:52:42 +0300

    resoırce

--- siparis/Controllers/HomeController.cs
+++ siparis/Controllers/HomeController.cs
@@ -17,9 +17,7 @@ namespace siparis.Controllers
         
         
         public ActionResult Index()
-        {
-            System.Threading.Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.CreateSpecificCulture("tr");
-
+        {            
             checkCart();           
             VdbSoftEntities db = new VdbSoftEntities(dbName);
             SortingPagingInfo info = new SortingPagingInfo();
