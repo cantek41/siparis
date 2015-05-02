dec91f857abbfcda1367f3967d34a7644cd7bb98
cantek41 <cantek41@gmail.com>
2015-05-02 10:35:59 +0300

    filter clas olusturuldı

---
+++ siparis/App_Start/sessiDataFilter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace siparis
+{
+    public class sessiDataFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var sessionlang = HttpContext.Current.Session["lang"];
+            if (sessionlang != null)
+            {
+                CultureInfo ci = CultureInfo.GetCultureInfo(sessionlang.ToString());
+                Thread.CurrentThread.CurrentUICulture = ci;
+                Thread.CurrentThread.CurrentUICulture = ci;
+            }
+
+
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName.ToLower().Trim();
+            string actionName = filterContext.ActionDescriptor.ActionName.ToLower().Trim();
+
+
+            if (!actionName.StartsWith("login") && !actionName.StartsWith("logoff") && !actionName.StartsWith("changelanguage"))
+            {
+                var session = HttpContext.Current.Session["profilim"];
+                HttpContext ctx = HttpContext.Current;
+                //Redirects user to login screen if session has timed out
+                if (session == null)
+                {
+                    base.OnActionExecuting(filterContext);
+
+
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                    {
+                        controller = "Account",
+                        action = "LogOff"
+                    }));
+
+                }
+            }
+        }
+
+    }
+}
--- siparis/Controllers/HomeController.cs
+++ siparis/Controllers/HomeController.cs
@@ -49,80 +49,71 @@ namespace siparis.Controllers
             return View(stokViewList(info));
         }
 
-
         public IndexDataViewModel stokViewList(SortingPagingInfo info)
         {
             VdbSoftEntities db = new VdbSoftEntities(dbName);
-            
-                IQueryable<STOKCARD> query = null;
-                switch (info.SortField)
-                {
-                    case "ID":
-                        query = (info.SortDirection == "ascending" ?
-                                 db.STOKCARDs.Where(x => x.ID != 0).Where(x => x.UPPER_CODE == x.CODE).OrderBy(c => c.ID) :
-                                 db.STOKCARDs.OrderByDescending(c => c.ID));
-                        break;
-                    //case "CompanyName":
-                    //    query = (info.SortDirection == "ascending" ?
-                    //             db.STOKCARDs.OrderBy(c => c.CompanyName) :
-                    //             db.STOKCARDs.OrderByDescending(c => c.CompanyName));
-                    //    break;
-                    //case "ContactName":
-                    //    query = (info.SortDirection == "ascending" ?
-                    //             db.STOKCARDs.OrderBy(c => c.ContactName) :
-                    //             db.STOKCARDs.OrderByDescending(c => c.ContactName));
-                    //    break;
-                    //case "Country":
-                    //    query = (info.SortDirection == "ascending" ?
-                    //             db.STOKCARDs.OrderBy(c => c.Country) :
-                    //             db.STOKCARDs.OrderByDescending(c => c.Country));
-                    //break;
-                }
-                query = query.Skip(info.CurrentPageIndex
-                      * info.PageSize).Take(info.PageSize);
-                ViewBag.SortingPagingInfo = info;
-                IndexDataViewModel data = new IndexDataViewModel();
-                data.stokcard = query.ToList();
-                data.stokgroup = db.STOKCATEGORies.ToList();
-                data.stokbrand = db.STOKBRANDs.ToList();
-                return data;
+
+            IQueryable<STOKCARD> query = null;
+            switch (info.SortField)
+            {
+                case "ID":
+                    query = (info.SortDirection == "ascending" ?
+                             db.STOKCARDs.Where(x => x.ID != 0).Where(x => x.UPPER_CODE == x.CODE).OrderBy(c => c.ID) :
+                             db.STOKCARDs.OrderByDescending(c => c.ID));
+                    break;
+                //case "CompanyName":
+                //    query = (info.SortDirection == "ascending" ?
+                //             db.STOKCARDs.OrderBy(c => c.CompanyName) :
+                //             db.STOKCARDs.OrderByDescending(c => c.CompanyName));
+                //    break;
+                //case "ContactName":
+                //    query = (info.SortDirection == "ascending" ?
+                //             db.STOKCARDs.OrderBy(c => c.ContactName) :
+                //             db.STOKCARDs.OrderByDescending(c => c.ContactName));
+                //    break;
+                //case "Country":
+                //    query = (info.SortDirection == "ascending" ?
+                //             db.STOKCARDs.OrderBy(c => c.Country) :
+                //             db.STOKCARDs.OrderByDescending(c => c.Country));
+                //break;
+            }
+            query = query.Skip(info.CurrentPageIndex
+                  * info.PageSize).Take(info.PageSize);
+            ViewBag.SortingPagingInfo = info;
+            IndexDataViewModel data = new IndexDataViewModel();
+            data.stokcard = query.ToList();
+            data.stokgroup = db.STOKCATEGORies.ToList();
+            data.stokbrand = db.STOKBRANDs.ToList();
+            return data;
 
 
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
             return View();
         }
-
         public ActionResult Contact()
         {
             // ViewBag.Message = "Your contact page.";
 
             return View();
         }
-
         public ActionResult NotFound()
         {
             return View();
         }
-
         public ActionResult CategorizedProducts(int Id)
         {
             TempData["Category"] = Id;
             return View(getDetailCategory(Id));
         }
-
-
-
         /// <summary>
         /// sayfa atlatma
         /// </summary>
         /// <param name="info"></param>
         /// <returns></returns>
         [HttpPost]
-
         public ActionResult CategorizedProducts([ModelBinder(typeof(DevExpressEditorsBinder))] IndexDataViewModel model, int Id)
         {
             int CategoryID = Id;
@@ -165,44 +156,6 @@ namespace siparis.Controllers
             return View(data);
 
         }
-
-
-        public List<STOKBRAND> BrandFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokbrand = (from brand in data.stokcard
-                              join x in db.STOKBRANDs on brand.BRAND_CODE equals x.BRAND_CODE
-                              select x).Distinct().ToList();
-            return data.stokbrand;
-        }
-
-        public List<STOKCOLOR> ColorFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokcolor = (from brand in data.stokcard
-                              join x in db.STOKCOLORs on brand.COLOR_CODE equals x.COLOR_CODE
-                              select x).Distinct().ToList();
-            return data.stokcolor;
-        }
-
-        public List<STOKSEASON> SeasonFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokseason = (from brand in data.stokcard
-                               join x in db.STOKSEASONs on brand.SEASON_CODE equals x.SEASON_CODE
-                               select x).Distinct().ToList();
-            return data.stokseason;
-        }
-        public List<STOKBODY> BodyFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokBody = (from brand in data.stokcard
-                             join x in db.STOKBODies on brand.BODY_CODE equals x.BODY_CODE
-                             select x).Distinct().ToList();
-            return data.stokBody;
-        }
-
-
         public IndexDataViewModel getDetailCategory(int CategoryId)
         {
             VdbSoftEntities db = new VdbSoftEntities(dbName);
@@ -231,45 +184,39 @@ namespace siparis.Controllers
 
         }
 
-    }
-
-
-    public class sessiDataFilter : ActionFilterAttribute
-    {
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        public List<STOKBRAND> BrandFilter(IndexDataViewModel data)
         {
-            var sessionlang = HttpContext.Current.Session["lang"];
-            if (sessionlang != null)
-            {
-                CultureInfo ci = CultureInfo.GetCultureInfo(sessionlang.ToString());
-                Thread.CurrentThread.CurrentUICulture = ci;
-                Thread.CurrentThread.CurrentUICulture = ci;
-            }
-
-
-            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName.ToLower().Trim();
-            string actionName = filterContext.ActionDescriptor.ActionName.ToLower().Trim();
-
-
-            if (!actionName.StartsWith("login") && !actionName.StartsWith("logoff") && !actionName.StartsWith("changelanguage"))
-            {
-                var session = HttpContext.Current.Session["profilim"];
-                HttpContext ctx = HttpContext.Current;
-                //Redirects user to login screen if session has timed out
-                if (session == null)
-                {
-                    base.OnActionExecuting(filterContext);
-
-
-                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
-                    {
-                        controller = "Account",
-                        action = "LogOff"
-                    }));
-
-                }
-            }
+            VdbSoftEntities db = new VdbSoftEntities(dbName);
+            data.stokbrand = (from brand in data.stokcard
+                              join x in db.STOKBRANDs on brand.BRAND_CODE equals x.BRAND_CODE
+                              select x).Distinct().ToList();
+            return data.stokbrand;
+        }
+        public List<STOKCOLOR> ColorFilter(IndexDataViewModel data)
+        {
+            VdbSoftEntities db = new VdbSoftEntities(dbName);
+            data.stokcolor = (from brand in data.stokcard
+                              join x in db.STOKCOLORs on brand.COLOR_CODE equals x.COLOR_CODE
+                              select x).Distinct().ToList();
+            return data.stokcolor;
+        }
+        public List<STOKSEASON> SeasonFilter(IndexDataViewModel data)
+        {
+            VdbSoftEntities db = new VdbSoftEntities(dbName);
+            data.stokseason = (from brand in data.stokcard
+                               join x in db.STOKSEASONs on brand.SEASON_CODE equals x.SEASON_CODE
+                               select x).Distinct().ToList();
+            return data.stokseason;
+        }
+        public List<STOKBODY> BodyFilter(IndexDataViewModel data)
+        {
+            VdbSoftEntities db = new VdbSoftEntities(dbName);
+            data.stokBody = (from brand in data.stokcard
+                             join x in db.STOKBODies on brand.BODY_CODE equals x.BODY_CODE
+                             select x).Distinct().ToList();
+            return data.stokBody;
         }
 
     }
+
 }
