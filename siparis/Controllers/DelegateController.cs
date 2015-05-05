880cfebc6d51a78c2d8927fd3d90c5d310a1b99b
cantek41 <cantek41@gmail.com>
2015-05-05 21:15:06 +0300

    delegete

---
+++ siparis/Controllers/DelegateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace siparis.Controllers
+{
+    public class DelegateController : Controller
+    {
+        //
+        // GET: /Delegate/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
