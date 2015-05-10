58eaafbdafde44bf9d5d88b638aa2a2905f8c77d
cantek41 <cantek41@gmail.com>
2015-05-10 19:55:33 +0300

    ana manu icerik yapıldı

--- siparis/Controllers/Filter.cs
+++ siparis/Controllers/Filter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
 using siparis.Models;
 
 namespace siparis.Controllers
@@ -13,6 +15,7 @@ namespace siparis.Controllers
         {
             this.dbName = dbName;
         }
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Client, NoStore = true)]
         public IndexDataViewModel getFilterItem()
         {
             VdbSoftEntities db = new VdbSoftEntities(dbName);
@@ -31,6 +34,7 @@ namespace siparis.Controllers
             data.stokbrand = db.STOKBRANDs.ToList();
             return data;
         }
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Client, NoStore = true)]
         public IndexDataViewModel getMainMenu()
         {
             using (VdbSoftEntities db = new VdbSoftEntities(dbName))
@@ -43,7 +47,8 @@ namespace siparis.Controllers
             };
         }
 
-        public List<STOKCARDViewModel> getStok(int count = -1)
+        [OutputCache(Duration = 3600, Location = OutputCacheLocation.Client, NoStore = true, VaryByParam = "count")]
+        public List<STOKCARDViewModel> getStok(int count)
         {
             string top = "";
             if (count != -1)
@@ -62,7 +67,7 @@ namespace siparis.Controllers
 
         public List<STOKCARDViewModel> getFilterStok(filterModel _filter)
         {
-            List<STOKCARDViewModel> data = getStok();
+            List<STOKCARDViewModel> data = getStok(-1);
             if (_filter.stokMainGroup != 0)
                 data = (from s in data
                         where s.MAIN_GRUP == _filter.stokMainGroup
@@ -121,123 +126,6 @@ namespace siparis.Controllers
             }
             return data;
 
-        }
-
-
-
-        public IndexDataViewModel getDetailFilter(IndexDataViewModel data)
-        {
-            data.stokMainGroup = MainFilter(data);
-            data.stokSubGroup = SubGroupFilter(data);
-            data.stokSubGroup2 = SubGroup2Filter(data);
-            data.stokCategory = CategoryFilter(data);
-            data.stokModel = ModelFilter(data);
-            data.stokRayon = RayonFilter(data);
-            data.stokSector = SectorFilter(data);
-            data.stokPacket = PacketFilter(data);
-            data.stokcolor = ColorFilter(data);
-            data.stokseason = SeasonFilter(data);
-            data.stokBody = BodyFilter(data);
-            data.stokbrand = BrandFilter(data);
-            return data;
-
-        }
-        public List<STOKMAINGROUP> MainFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokMainGroup = (from brand in data.stokcard
-                                  join x in db.STOKMAINGROUPs on brand.MAIN_GRUP equals x.ID
-                                  select x).Distinct().ToList();
-            return data.stokMainGroup;
-        }
-        public List<STOKSUBGROUP> SubGroupFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokSubGroup = (from brand in data.stokcard
-                                 join x in db.STOKSUBGROUPs on brand.SUB_GRUP1 equals x.ID
-                                 select x).Distinct().ToList();
-            return data.stokSubGroup;
-        }
-        public List<STOKSUBGROUP2> SubGroup2Filter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokSubGroup2 = (from brand in data.stokcard
-                                  join x in db.STOKSUBGROUP2 on brand.SUB_GRUP2 equals x.ID
-                                  select x).Distinct().ToList();
-            return data.stokSubGroup2;
-        }
-        public List<STOKCATEGORY> CategoryFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokCategory = (from brand in data.stokcard
-                                 join x in db.STOKCATEGORies on brand.CATEGORY_CODE equals x.STOK_GROUP_CODE
-                                 select x).Distinct().ToList();
-            return data.stokCategory;
-        }
-        public List<STOKMODEL> ModelFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokModel = (from brand in data.stokcard
-                              join x in db.STOKMODELs on brand.MODEL_CODE equals x.MODEL_CODE
-                              select x).Distinct().ToList();
-            return data.stokModel;
-        }
-        public List<STOKRAYON> RayonFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokRayon = (from brand in data.stokcard
-                              join x in db.STOKRAYONs on brand.RAYON_CODE equals x.RAYON_CODE
-                              select x).Distinct().ToList();
-            return data.stokRayon;
-        }
-        public List<STOKSECTOR> SectorFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokSector = (from brand in data.stokcard
-                               join x in db.STOKSECTORs on brand.SECTOR_CODE equals x.SECTOR_CODE
-                               select x).Distinct().ToList();
-            return data.stokSector;
-        }
-        public List<STOKPACKET> PacketFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokPacket = (from brand in data.stokcard
-                               join x in db.STOKPACKETs on brand.PACK_CODE equals x.PACKET_CODE
-                               select x).Distinct().ToList();
-            return data.stokPacket;
-        }
-
-        public List<STOKBRAND> BrandFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokbrand = (from brand in data.stokcard
-                              join x in db.STOKBRANDs on brand.BRAND_CODE equals x.BRAND_CODE
-                              select x).Distinct().ToList();
-            return data.stokbrand;
-        }
-        public List<STOKCOLOR> ColorFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokcolor = (from brand in data.stokcard
-                              join x in db.STOKCOLORs on brand.COLOR_CODE equals x.COLOR_CODE
-                              select x).Distinct().ToList();
-            return data.stokcolor;
-        }
-        public List<STOKSEASON> SeasonFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokseason = (from brand in data.stokcard
-                               join x in db.STOKSEASONs on brand.SEASON_CODE equals x.SEASON_CODE
-                               select x).Distinct().ToList();
-            return data.stokseason;
-        }
-        public List<STOKBODY> BodyFilter(IndexDataViewModel data)
-        {
-            VdbSoftEntities db = new VdbSoftEntities(dbName);
-            data.stokBody = (from brand in data.stokcard
-                             join x in db.STOKBODies on brand.BODY_CODE equals x.BODY_CODE
-                             select x).Distinct().ToList();
-            return data.stokBody;
         }
     }
 }
